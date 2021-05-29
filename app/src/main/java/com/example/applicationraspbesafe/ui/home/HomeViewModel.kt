799643d3ecc48ea11121d4097abe22ca0cd20098
lucas <lucasdouillard@hotmail.fr>
2021-05-29 20:24:32 +0200

    Initial commit

---
+++ app/src/main/java/com/example/applicationraspbesafe/ui/home/HomeViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.applicationraspbesafe.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "Page d'acceuil de l'application RaspBeSafe"
+    }
+    val text: LiveData<String> = _text
+}
